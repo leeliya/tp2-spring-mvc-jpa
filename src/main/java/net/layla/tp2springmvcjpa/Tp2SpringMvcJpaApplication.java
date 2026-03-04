a1919bf6f0252f6a5cea55a38749186dc7555b66
layla <laylaelhajjaji@gmail.com>
2026-03-04 15:30:02 +0000

    Initial commit - Spring Boot project setup

---
+++ src/main/java/net/layla/tp2springmvcjpa/Tp2SpringMvcJpaApplication.java
@@ -0,0 +1,13 @@
+package net.layla.tp2springmvcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2SpringMvcJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2SpringMvcJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/layla/tp2springmvcjpa/Tp2SpringMvcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.layla.tp2springmvcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2SpringMvcJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
