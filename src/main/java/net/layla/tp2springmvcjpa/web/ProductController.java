f066f07cff50c97d3602bf04c7638dee6255fe51
layla <laylaelhajjaji@gmail.com>
2026-03-30 11:21:06 +0100

    Lister tous les produits

--- src/main/java/net/layla/tp2springmvcjpa/web/ProductController.java
+++ src/main/java/net/layla/tp2springmvcjpa/web/ProductController.java
@@ -1,9 +1,13 @@
 package net.layla.tp2springmvcjpa.web;
 
+import net.layla.tp2springmvcjpa.entities.Product;
 import net.layla.tp2springmvcjpa.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -15,7 +19,9 @@ public class ProductController {
     }
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList", products);
         return "products" ;
     }
 }
