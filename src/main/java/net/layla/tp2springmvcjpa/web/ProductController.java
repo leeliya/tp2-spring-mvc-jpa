511816aae2590a6af0246c59f56c1f89452ec956
layla <laylaelhajjaji@gmail.com>
2026-04-08 18:02:47 +0100

    Ajout de méthode saveProduct

--- src/main/java/net/layla/tp2springmvcjpa/web/ProductController.java
+++ src/main/java/net/layla/tp2springmvcjpa/web/ProductController.java
@@ -1,11 +1,15 @@
 package net.layla.tp2springmvcjpa.web;
 
+import jakarta.validation.Valid;
 import net.layla.tp2springmvcjpa.entities.Product;
 import net.layla.tp2springmvcjpa.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -41,5 +45,12 @@ public class ProductController {
     public String newProduct (Model model) {
         model.addAttribute("product", new Product());
         return "new-product" ;
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct (@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "new-product" ;
+        productRepository.save(product);
+        return "redirect:/index" ;
     }
 }
